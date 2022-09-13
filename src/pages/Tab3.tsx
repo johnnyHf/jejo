3f9ba95afa60611004afd680fc053555c4ca034b
huangfeng <1247959612@qq.com>
2022-09-13 21:59:59 +0800

    新增axios

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,25 +1,33 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/react';
 import './Tab3.css';
-
+import axios from 'axios';
+import ExploreContainer from "../components/ExploreContainer";
 const Tab3: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
-      </IonContent>
-    </IonPage>
-  );
+    let nnq = "";
+
+    axios.get("http://localhost:8080/hao123").then((data) => {
+        nnq = JSON.stringify(data.data).toString();
+        console.log(nnq)
+    }).catch(err => {
+        console.log(err)
+    })
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Tab 3</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large"></IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <ExploreContainer name="1212"></ExploreContainer>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Tab3;
